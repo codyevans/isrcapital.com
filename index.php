8a963ef6644997353dd6cf5895185b47e70b2cb9
Cody Evans <codyevansdesign@gmail.com>
2013-08-06 13:14:58 -0600

    added to terms

--- index.php
+++ index.php
@@ -29,6 +29,43 @@
 			background: #C50000;
 			color:#fff;
 		}
+
+		#terms h5 {
+			font-size: 14px;
+		}
+
+		#small-terms h5 {
+			font-size: 12px;
+			margin-top: 12px;
+		}
+
+		#terms p {
+ 			font-size: 11px;
+ 			line-height: 1.5em;
+		}
+
+		#small-terms p {
+			font-size: 9px;
+			margin-bottom: 0px;
+			margin-top: 6px;
+			line-height: 1.75em;
+		}
+
+		#hero {
+			display: none;
+		}
+
+		.container {
+			margin-top: 20px;
+		}
+
+		h5#terms-label {
+			margin-bottom: 20px;
+		}
+
+		header {
+			border-bottom: 1px solid rgba(0,0,0,0.2);
+		}
 	</style>
 
 	<!--[if lt IE 9]>
@@ -86,7 +123,7 @@
 <!--header hero images-->
 	<div id="hero">
 
-<div class="herowidth">
+	<div class="herowidth">
 			<img src="images/header/hero.jpg" alt="Hero Image Six">
 			<img src="images/header/hero.jpg" alt="Hero Image Six">
 		</div>
@@ -98,76 +135,112 @@
 			<h2>Terms and Conditions</h2>
 			<p>Please agree to the terms and conditions below to continue.</p>
 			<div id="terms">
-	          <h5>CONFIDENTIALITY NOTICE</h5>
-	          <p>
-	            This presentation contains legally privileged, confidential information belonging to The ISR Capital,
-	            LLC, et al and or affiliated entities (the "Company"). The information is intended only for the use of: 
-	            Registered and Qualified Invitees, of the Company. If you are not the intended recipient, you are hereby
-	            notified that any disclosure, copying, distribution or taking any action based on the contents of this 
-	            presentation is strictly prohibited. If you have received this in error, please contact sender and delete 
-	            all copies. Also, please be aware that if you are not the intended recipient, any review, disclosure, copying, 
-	            distribution or any action or reliance based on this message is prohibited by law.
-	          </p>
-	          <h5>FORWARD LOOKING STATEMENTS</h5>
-	          <p>
-	            This website or its presentation contains “forward-looking statements”. These forward-looking statements are made
-	            as of the date of this presentation and The ISR Capital, LLC, et al and or affiliated entities (the "Company") does
-	            not intend, and does not assume any obligation, to update these forward-looking statements. Forward-looking statements
-	            include, but are not limited to, statements with respect to the timing and amount of estimated future exploration, 
-	            success of activities, expenditures, permitting, entitlements, profits and requirements for additional capital and 
-	            access to data. Forward-looking statements involve known and unknown risks, uncertainties and other factors which may 
-	            cause the actual results, performance or achievements of the Company to be materially different from any future results, 
-	            performance or achievements expressed or implied by the forward-looking statements. Such factors include, among others, r
-	            isks related to actual results of current exploration activities; changes in project parameters as plans continue to be 
-	            refined; the ability to enter into joint ventures or to acquire or dispose; changes in permitting, environmental policy, 
-	            and or endangered species; changes in ownership of property interests; future prices of mineral resources; accidents, labor
-	            disputes and other risks of the energy, banking, mining industry, and Resort real estate markets; ability to obtain financing;
-	            and delays in obtaining governmental approvals or financing. Any forward-looking statements related to analyses and other data 
-	            and are forecasts of future results and estimates of amounts not yet determinable. Although we believe that our plans and 
-	            expectations reflected in this presentation are reasonable, we can not assure that such plans or expectations will be achieved. 
-	          </p>
-	          <h5>TERMS AND CONDITIONS</h5>
-	          <p>
-	            The ISR Capital, LLC, and or affiliated entities (the "Company") maintains this site (the "Site") for your personal use, 
-	            information, education and communication. The Company invites you to browse the Site, but please read these terms and conditions 
-	            before doing so. 
-	            General
-	          </p>
-	          <p>
-	            Your access to and use of the Site is subject to the following terms and conditions ("Terms and Conditions"). The Company 
-	            may revise these Terms and Conditions at any time by updating this posting. You are bound by any such revisions and should, 
-	            therefore, periodically visit this page to review the current Terms and Conditions to which you are bound. By accessing and 
-	            browsing the Site, you signify your acceptance of these Terms and Conditions without limitation or qualification. 
-	          </p>
-	          <h5>LEGAL DISCLOSURES</h5>
-	          <p>
-	            The features and amenities described and depicted herein are based upon current development plans. Because the plan is 
-	            evolving, the described plan is tentative and conceptual only and subject to change by the Company in its sole discretion 
-	            without notice. Actual development may not be as currently proposed. Further, the Company is not obligated to develop any
-	            specific future facility at any specific and, subject to any governmental land use regulation or other planning considerations,
-	            may develop any of the future facilities at any location.
-	          </p>
-	          <p>
-	            Any of the described plans must be considered planned, but not promised. All content, including prices and availability, 
-	            is for information purposes only and is subject to change without prior notice. Not all photos are on property, or owned by 
-	            the Company. This material shall not constitute an offer or solicitation in any jurisdiction where the legal requirements for 
-	            such an offering have not been met. 
-	          </p>
-	          <h5>RESTRICTIONS ON USE OF COPYRIGHTS/TRADEMARKS</h5>
-	          <p>
-	            This Site contains graphics, designs, software, text, names, logos, images, copyrights and other proprietary materials owned 
-	            by the Company or its affiliates, and used by the Company throughout the country (the "Trademarks"). You should assume that all
-	            materials and Trademarks contained in the Site are either the copyrighted or registered property of the Company or are the 
-	            copyrighted or registered property of third parties. Unauthorized use of the materials and Trademarks is strictly prohibited
-	            and is a violation of the rights of the Company and/or third parties. 
-	          </p>
-	          <p>
-	            The Company and accompanying design logo are Trademarks of the Company. You may not use, copy, display, distribute, or 
-	            reproduce any of the Trademarks in any way except as expressly authorized pursuant to a written Trademark use agreement. 
-	            In the event that you misuse any Trademark owned by the Company, the Company will aggressively enforce its intellectual property 
-	            rights to the fullest extent of the law. WARNING: SOME STATE, AND OR FEDERAL DEPARTMENTS HAVE NOT INSPECTED, EXAMINED, OR QUALIFIED
-	            THIS OFFERING.
-	          </p>
+			  <h5>DISCLAIMERS</h5>
+			  <P>
+			  	All website information and the website itself are provided "as is" and "as available" without any warranty of any kind. We make no representations or warranties. To the fullest extent permitted by law, we hereby expressly disclaim any and all express or implied representations and warranties of any kind to you and/or any third party including, without limitation, any warranty as to merchantability or fitness for a particular purpose, non-infringement, the use or inability to use the website, or the correctness, quality, accuracy, reliability, performance, completeness, timeliness or continued availability of the website. Without limiting the foregoing, we make no representation or warranty that the website, including the website information, will conform to any description thereof or perform any desired operations or functions, be uninterrupted or error-free, or be free of viruses, or other disabling or harmful components.
+			  </P>
+			  <P>
+			  	We do not represent or endorse and hereby disclaim any obligation or responsibility to verify the correctness, quality, accuracy, reliability, performance, completeness, timeliness, or continued availability of any of the website information or advertisements contained on, distributed through or linked, downloadable, or accessible from the website. You acknowledge that if you rely on any website information, you do so at your sole and exclusive risk.  COMPANY reserves the right (but shall have no obligation) to make improvements and changes and to discontinue any portion of the website.
+			  </P>
+			  <P>
+			  	The website may contain hyperlinks to sites operated by persons other than COMPANY. Such hyperlinks are provided for your reference and convenience only. These linked sites are not under COMPANY control and may be governed by their own terms of use. We have not developed or reviewed the content appearing in or on such linked sites and you agree not to hold us responsible for the content or operation of such linked sites.  COMPANY does not endorse any company or product to which it links. A hyperlink does not and should not be deemed to imply that COMPANY in any way endorses or supports the content, operator or owner of such linked sites. You understand that the linked sites may contain illegal or objectionable content or other disabling or harmful components. If you decide to access any third party sites linked to this site, you do so at your sole and exclusive risk. You assume, and COMPANY disclaims, all responsibility for any loss or injury sustained by you as a user of any linked site.
+			  </P>
+			  <P>
+			  	In no event will we be liable to you or any third party for any loss, damage or injury (including, without limitation, any loss or injury caused, in whole or in part, by us with respect to any website information) in tort, contract, strict liability or otherwise except for liability arising out of gross negligence or willful misconduct. Notwithstanding the foregoing, in no event will we be liable to you or any third party for any lost profits, lost opportunity, indirect, special, exemplary, consequential, incidental or punitive damages whatsoever, even if we have been advised of the possibility of such damages.
+			  </P>
+			  <P>
+			  	You agree that you will not forward or distribute any of the website information to any other person or copy the website information in any manner whatsoever, in each case, without COMPANY prior written consent. Any distribution, forwarding or copying of the website information, in whole or in part, without COMPANY prior written consent is unauthorized and strictly prohibited.
+			  </P>
+			  <P>
+			  	You agree to indemnify, defend and hold us harmless, from any loss, damage or injury (including reasonable attorneys' fees) suffered, incurred or asserted as a result of (a) any use or misuse by you of the website, (b) any actions related to your receipt and use of the website information, whether or not authorized, (c) any infringement by you of any of our intellectual property rights, (d) your breach of any of the provisions contained herein, and (e) claims asserted by third parties which, if proven, would place you in breach of any of the provisions contained herein.
+			  </P>
+			  <p>
+			  	Some countries may provide rights in addition to those above or may not allow the exclusion or limitation of implied warranties or liability for incidental or consequential damages. Therefore, some of the above limitations may not apply to you, or there may be provisions of applicable law that supersede the above.
+			  </p>
+			  <p>
+			  	Any legal notice or disclaimer (or portion thereof) declared invalid is severable and shall not affect the validity or enforceability of the remainder of the disclaimers. The terms of the disclaimers are governed by the laws of the state of IDAHO, without regard to conflicts of law principles.
+			  </p>
+			  <p>
+			  	<strong>NOTE: COMPANY. http://idahostateregionalcenter.com/ is not a Securities Dealer nor does it affect the Sale of Securities.</strong>  This web site is NOT intended as a prospectus to sell securities. It is an informational site with no warranties or representations expressed or implied. 
+			  </p>
+
+			  <br>
+			  <hr>
+
+			  <h5 id="terms-label">Terms and Conditions</h5>
+
+			  <div id="small-terms">
+		          <h5>CONFIDENTIALITY NOTICE</h5>
+		          <p>
+		            This presentation contains legally privileged, confidential information belonging to The ISR Capital,
+		            LLC, et al and or affiliated entities (the "Company"). The information is intended only for the use of: 
+		            Registered and Qualified Invitees, of the Company. If you are not the intended recipient, you are hereby
+		            notified that any disclosure, copying, distribution or taking any action based on the contents of this 
+		            presentation is strictly prohibited. If you have received this in error, please contact sender and delete 
+		            all copies. Also, please be aware that if you are not the intended recipient, any review, disclosure, copying, 
+		            distribution or any action or reliance based on this message is prohibited by law.
+		          </p>
+		          <h5>FORWARD LOOKING STATEMENTS</h5>
+		          <p>
+		            This website or its presentation contains “forward-looking statements”. These forward-looking statements are made
+		            as of the date of this presentation and The ISR Capital, LLC, et al and or affiliated entities (the "Company") does
+		            not intend, and does not assume any obligation, to update these forward-looking statements. Forward-looking statements
+		            include, but are not limited to, statements with respect to the timing and amount of estimated future exploration, 
+		            success of activities, expenditures, permitting, entitlements, profits and requirements for additional capital and 
+		            access to data. Forward-looking statements involve known and unknown risks, uncertainties and other factors which may 
+		            cause the actual results, performance or achievements of the Company to be materially different from any future results, 
+		            performance or achievements expressed or implied by the forward-looking statements. Such factors include, among others, r
+		            isks related to actual results of current exploration activities; changes in project parameters as plans continue to be 
+		            refined; the ability to enter into joint ventures or to acquire or dispose; changes in permitting, environmental policy, 
+		            and or endangered species; changes in ownership of property interests; future prices of mineral resources; accidents, labor
+		            disputes and other risks of the energy, banking, mining industry, and Resort real estate markets; ability to obtain financing;
+		            and delays in obtaining governmental approvals or financing. Any forward-looking statements related to analyses and other data 
+		            and are forecasts of future results and estimates of amounts not yet determinable. Although we believe that our plans and 
+		            expectations reflected in this presentation are reasonable, we can not assure that such plans or expectations will be achieved. 
+		          </p>
+		          <h5>TERMS AND CONDITIONS</h5>
+		          <p>
+		            The ISR Capital, LLC, and or affiliated entities (the "Company") maintains this site (the "Site") for your personal use, 
+		            information, education and communication. The Company invites you to browse the Site, but please read these terms and conditions 
+		            before doing so. 
+		            General
+		          </p>
+		          <p>
+		            Your access to and use of the Site is subject to the following terms and conditions ("Terms and Conditions"). The Company 
+		            may revise these Terms and Conditions at any time by updating this posting. You are bound by any such revisions and should, 
+		            therefore, periodically visit this page to review the current Terms and Conditions to which you are bound. By accessing and 
+		            browsing the Site, you signify your acceptance of these Terms and Conditions without limitation or qualification. 
+		          </p>
+		          <h5>LEGAL DISCLOSURES</h5>
+		          <p>
+		            The features and amenities described and depicted herein are based upon current development plans. Because the plan is 
+		            evolving, the described plan is tentative and conceptual only and subject to change by the Company in its sole discretion 
+		            without notice. Actual development may not be as currently proposed. Further, the Company is not obligated to develop any
+		            specific future facility at any specific and, subject to any governmental land use regulation or other planning considerations,
+		            may develop any of the future facilities at any location.
+		          </p>
+		          <p>
+		            Any of the described plans must be considered planned, but not promised. All content, including prices and availability, 
+		            is for information purposes only and is subject to change without prior notice. Not all photos are on property, or owned by 
+		            the Company. This material shall not constitute an offer or solicitation in any jurisdiction where the legal requirements for 
+		            such an offering have not been met. 
+		          </p>
+		          <h5>RESTRICTIONS ON USE OF COPYRIGHTS/TRADEMARKS</h5>
+		          <p>
+		            This Site contains graphics, designs, software, text, names, logos, images, copyrights and other proprietary materials owned 
+		            by the Company or its affiliates, and used by the Company throughout the country (the "Trademarks"). You should assume that all
+		            materials and Trademarks contained in the Site are either the copyrighted or registered property of the Company or are the 
+		            copyrighted or registered property of third parties. Unauthorized use of the materials and Trademarks is strictly prohibited
+		            and is a violation of the rights of the Company and/or third parties. 
+		          </p>
+		          <p>
+		            The Company and accompanying design logo are Trademarks of the Company. You may not use, copy, display, distribute, or 
+		            reproduce any of the Trademarks in any way except as expressly authorized pursuant to a written Trademark use agreement. 
+		            In the event that you misuse any Trademark owned by the Company, the Company will aggressively enforce its intellectual property 
+		            rights to the fullest extent of the law. WARNING: SOME STATE, AND OR FEDERAL DEPARTMENTS HAVE NOT INSPECTED, EXAMINED, OR QUALIFIED
+		            THIS OFFERING.
+		          </p>
+		      </div> <!-- /small terms -->
 	        </div>
 
 	        <!-- TERM AGREE FORM -->
